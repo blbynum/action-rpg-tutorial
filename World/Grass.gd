9be91141012c0a0598b812e0ba04a13842e4ecf6
Ben Bynum <ben@bbynum.com>
2023-03-12 18:45:26 -0400

    add esc to exit; animate grass death

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,17 +1,13 @@
 extends Node2D
 
+var GrassEffect = preload("res://Effects/GrassEffect.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Attack"):
+		var grassEffect = GrassEffect.instance()
+		var world = get_tree().current_scene
+		world.add_child(grassEffect)
+		grassEffect.global_position = global_position
 		queue_free()
