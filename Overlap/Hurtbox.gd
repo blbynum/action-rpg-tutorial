01d683081eff462a4291ac10f78d5e8b7c843313
Ben Bynum <ben@bbynum.com>
2023-03-18 15:02:51 -0400

    Add running, add dodge on roll; add invincibility export

--- Overlap/Hurtbox.gd
+++ Overlap/Hurtbox.gd
@@ -2,23 +2,40 @@ extends Area2D
 
 const HitEffect = preload("res://Effects/HitEffect.tscn")
 
-var invincible = false setget set_invincible
+var hurtCooldown = false setget set_hurt_cooldown
 
 onready var timer = $Timer
 onready var collisionShape = $CollisionShape2D
 
-signal invincibility_started
-signal invincibility_ended
+export var invincibile = false setget set_invincibility
 
-func set_invincible(value):
-	invincible = value
-	if invincible == true:
-		emit_signal("invincibility_started")
+signal hurt_cooldown_started
+signal hurt_cooldown_ended
+
+func set_hurt_cooldown(value):
+	hurtCooldown = value
+	if hurtCooldown == true:
+		emit_signal("hurt_cooldown_started")
 	else:
-		emit_signal("invincibility_ended")
+		emit_signal("hurt_cooldown_ended")
 
-func start_invincibility(duration):
-	self.invincible = true
+func start_dodge():
+	if invincibile == false:
+		collisionShape.set_deferred("disabled", true)
+
+func end_dodge():
+	if invincibile == false:
+		collisionShape.set_deferred("disabled", false)
+	
+func set_invincibility(value):
+	invincibile = value
+	if invincibile == true:
+		collisionShape.set_deferred("disabled", true)
+	else:
+		collisionShape.set_deferred("disabled", false)
+
+func start_hurt_cooldown(duration):
+	self.hurtCooldown = true
 	timer.start(duration)
 
 func create_hit_effect():
@@ -28,12 +45,13 @@ func create_hit_effect():
 	effect.global_position = global_position
 
 func _on_Timer_timeout():
-	self.invincible = false
+	self.hurtCooldown = false
 
+func _on_Hurtbox_hurt_cooldown_started():
+	if invincibile == false:
+		collisionShape.set_deferred("disabled", true)
 
-func _on_Hurtbox_invincibility_started():
-	collisionShape.set_deferred("disabled", true)
+func _on_Hurtbox_hurt_cooldown_ended():
+	if invincibile == false:
+		collisionShape.set_deferred("disabled", false)
 
-
-func _on_Hurtbox_invincibility_ended():
-	collisionShape.set_deferred("disabled", false)
--- Player/Player.gd
+++ Player/Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 const PlayerHurtSound = preload("res://Player/PlayerHurtSound.tscn")
 
 export var ACCELERATION = 500
+export var RUN_ACCELERATION = 700
 export var MAX_SPEED = 80
+export var RUN_SPEED = 120
 export var ROLL_SPEED = 120
 export var FRICTION = 500
 
@@ -51,6 +53,15 @@ func _physics_process(delta):
 	
 
 func move_state(delta):
+	var speed = MAX_SPEED
+	var acceleration = ACCELERATION
+	if Input.is_action_pressed("run"):
+		speed = RUN_SPEED
+		acceleration = RUN_ACCELERATION
+		
+#	if Input.is_action_just_released("run"):
+		
+	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -64,7 +75,7 @@ func move_state(delta):
 		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+		velocity = velocity.move_toward(input_vector * speed, acceleration * delta)
 		
 		# more customizable - different feel
 		#velocity += input_vector * ACCELERATION * delta
@@ -102,14 +113,14 @@ func attack_animation_finished():
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
-	hurtbox.start_invincibility(0.6)
+	hurtbox.start_hurt_cooldown(0.6)
 	hurtbox.create_hit_effect()
 	var playerHurtSound = PlayerHurtSound.instance()
 	get_tree().current_scene.add_child(playerHurtSound)
-	
-func _on_Hurtbox_invincibility_started():
+
+func _on_Hurtbox_hurt_cooldown_started():
 	blinkAnimationPlayer.play("Start")
 
 
-func _on_Hurtbox_invincibility_ended():
+func _on_Hurtbox_hurt_cooldown_ended():
 	blinkAnimationPlayer.play("Stop")
